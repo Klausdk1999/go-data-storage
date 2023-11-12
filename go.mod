d094095d50b8d13fe263b143e7806ab7f738e8d7
Klaus Dieter Kupper <klausdk1999@gmail.com>
2023-11-11 22:11:24 -0300

    feat: hanlde cors for local tests

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module data-storage
 go 1.21.4
 
 require github.com/lib/pq v1.10.9
+
+require github.com/rs/cors v1.10.1 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"github.com/rs/cors"
 
 	_ "github.com/lib/pq"
 )
@@ -17,8 +18,25 @@ const (
 )
 
 func main() {
-    http.HandleFunc("/users", UsersHandler)
-    http.HandleFunc("/readings", ReadingsHandler)
-    http.HandleFunc("/user_readings", UserReadingsHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    mux := http.NewServeMux()
+    
+    // Register your handlers
+    mux.HandleFunc("/users", UsersHandler)
+    mux.HandleFunc("/readings", ReadingsHandler)
+    mux.HandleFunc("/user_readings", UserReadingsHandler)
+
+    // Setup CORS
+    c := cors.New(cors.Options{
+        AllowedOrigins: []string{"http://localhost:3000"}, // Adjust this to your frontend's URL
+        AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowedHeaders: []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
+        // Enable Debugging for testing, consider disabling in production
+        Debug: true,
+    })
+
+    // Apply CORS middleware
+    handler := c.Handler(mux)
+
+    // Start server with CORS handler
+    log.Fatal(http.ListenAndServe(":8080", handler))
 }
