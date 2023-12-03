1622d7449291a59d915caf9b0573bfd389c13920
Klaus Dieter Kupper <klausdk1999@gmail.com>
2023-12-03 16:00:10 -0300

    Add error logging and handling in getAllUsers function

--- users_handler.go
+++ users_handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -18,10 +19,15 @@ func UsersHandler(w http.ResponseWriter, r *http.Request) {
 
 func getAllUsers(w http.ResponseWriter, r *http.Request) {
     db := OpenConnection()
+    if db == nil {
+        http.Error(w, "Failed to open database connection", http.StatusInternalServerError)
+        return
+    }
     defer db.Close()
 
-    rows, err := db.Query("SELECT id, name, rfid, categoria, matricula FROM users")
+    rows, err := db.Query("SELECT id, name, categoria, matricula, rfid FROM users")
     if err != nil {
+        log.Printf("Database query error: %v", err)
         http.Error(w, "Database query error", http.StatusInternalServerError)
         return
     }
@@ -30,16 +36,25 @@ func getAllUsers(w http.ResponseWriter, r *http.Request) {
     var users []User
     for rows.Next() {
         var user User
-        err := rows.Scan(&user.ID, &user.Name, &user.Rfid, &user.Categoria, &user.Matricula)
+        err := rows.Scan(&user.ID, &user.Name, &user.Categoria, &user.Matricula, &user.Rfid)
         if err != nil {
+            log.Printf("Error scanning users: %v", err)
             http.Error(w, "Error scanning users", http.StatusInternalServerError)
             return
         }
         users = append(users, user)
     }
 
+    // Check for errors during iteration
+    if err = rows.Err(); err != nil {
+        log.Printf("Error iterating over rows: %v", err)
+        http.Error(w, "Error iterating over rows", http.StatusInternalServerError)
+        return
+    }
+
     usersBytes, err := json.MarshalIndent(users, "", "\t")
     if err != nil {
+        log.Printf("Error marshaling users: %v", err)
         http.Error(w, "Error marshaling users", http.StatusInternalServerError)
         return
     }
