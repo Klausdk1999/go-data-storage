d8d2c83c229f396190a0f1d771544e4b6dc0268a
cissavgonc <ceciliavgonc12@gmail.com>
2023-12-06 04:04:34 -0300

    user readings versao inicial

--- user_readings_handler.go
+++ user_readings_handler.go
@@ -34,7 +34,7 @@ func UserReadingsHandler(w http.ResponseWriter, r *http.Request) {
     }
 
     // Query database for readings belonging to the user
-    rows, err := db.Query("SELECT id, userid, timestamp, value, torquevalues, motionwastes, setvalue, asmtimes FROM readings WHERE user_id = $1", userID)
+    rows, err := db.Query("SELECT id, userid, timestamp, value FROM readings WHERE user_id = $1", userID)
     if err != nil {
         http.Error(w, "Database query error", http.StatusInternalServerError)
         return
@@ -44,7 +44,7 @@ func UserReadingsHandler(w http.ResponseWriter, r *http.Request) {
  var readings []Reading
     for rows.Next() {
         var reading Reading
-        err := rows.Scan(&reading.ID, &reading.UserID, &reading.Timestamp, &reading.Value, pq.Array(&reading.TorqueValues), pq.Array(&reading.MotionWastes), &reading.SetValue, pq.Array(&reading.AsmTimes))
+        err := rows.Scan(&reading.ID, &reading.UserID, &reading.Timestamp, &reading.Value)
 
         if err != nil {
             http.Error(w, "Error scanning readings", http.StatusInternalServerError)
